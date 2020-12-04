a001238ec4b2d8ce279b353a77daf06cab316149
Slowwie <michaelbrauner82@gmail.com>
2020-12-04 17:51:52 +0100

    Added wordbreak to result items. Closes #7

--- resources/views/components/input/base-input-container.blade.php
+++ resources/views/components/input/base-input-container.blade.php
@@ -9,7 +9,7 @@
 
     @if($label)
         <label for="{{$id}}"
-               class="{{$inline ? 'w-2/6' : 'block'}} text-sm leading-5 font-medium text-gray-700">{{$label}}</label>
+               class="{{$inline ? 'w-2/6' : 'block'}} text-sm leading-5 font-medium text-gray-700 break-words">{{$label}}</label>
     @endif
 
     <div class="{{$inline ? 'w-4/6' : 'block w-full'}} relative">
